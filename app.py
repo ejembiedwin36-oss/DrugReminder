62c6e67e3c0aeee5b2afaedf99fe3ee38d05c002
ejembi <ejembiedwin36@gmail.com>
2026-09-09 14:50:40 +0100

    drug reminder

--- app.py
+++ app.py
@@ -17,6 +17,10 @@ def register_hospital():
 
     if request.method == "POST":
         hospital_name = request.form["hospital_name"]
+        hospital_type = request.form["hospital_type"]
+        phone = request.form["phone_number"]
+        email = request.form["email"]
+        address = request.form["address"]
         return hospital_name
 
     else:
